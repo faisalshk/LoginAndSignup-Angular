c1431de89c6c8e5fd0222bfa7044110b2a46de86
faisalshk <86406064+faisalshk@users.noreply.github.com>
2024-02-03 22:11:45 +0530

    Bug fixing

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -15,17 +16,15 @@ export class AccountComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    // Load user data from localStorage on component initialization
     const userdata = localStorage.getItem('users');
     if (userdata) this.userData = JSON.parse(userdata);
 
-    // Load current user data from localStorage on component initialization
     const currentuser = localStorage.getItem('currentUser');
     if (currentuser) {
       this.currentUser = JSON.parse(currentuser);
+      console.log(this.currentUser);
     }
 
-    // Initialize form group with form controls
     this.user = new FormGroup({
       userName: new FormControl(),
       Email: new FormControl(),
@@ -33,53 +32,28 @@ export class AccountComponent implements OnInit {
     });
   }
 
-  // Method to edit user profile
   editProfile(user: FormGroup) {
-    // Find user data to be edited based on current user's email
     const editData = this.userData.find(
-      (userData) => userData.Email === this.currentUser.Email
+      (user) => user.Email === this.currentUser.Email
     );
 
-    // Check if email, username, or password has been modified
-    const emailModified = this.user.value.Email !== this.currentUser.Email;
-    const usernameModified =
-      this.user.value.userName !== this.currentUser.userName;
-    const passwordModified =
-      this.user.value.password !== this.currentUser.password;
+    editData.Email = this.user.value.Email || this.currentUser.Email;
+    editData.userName = this.user.value.userName || this.currentUser.userName;
+    editData.password = this.user.value.password || this.currentUser.password;
 
-    // Update user data and display appropriate alerts for each modified field
-    if (emailModified) {
-      editData.Email = this.user.value.Email || this.currentUser.Email;
-      alert('Email Updated!!');
-    }
-    if (usernameModified) {
-      editData.userName = this.user.value.userName || this.currentUser.userName;
-      alert('Username Updated!!');
-    }
-    if (passwordModified) {
-      editData.password = this.user.value.password || this.currentUser.password;
-      alert('Password Updated');
-    }
+    this.currentUser = editData;
 
-    // Display an alert if all fields are modified
-    if (emailModified && usernameModified && passwordModified) {
-      alert('All the details are updated successfully');
-    }
-
-    // Update user data in localStorage
     localStorage.setItem('users', JSON.stringify(this.userData));
 
-    // Update current user data in localStorage
-    localStorage.setItem('currentUser', JSON.stringify(this.userData));
+    localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
 
-    // Reset form after editing profile
+    alert('Details updated successfully');
+
     this.user.reset();
   }
 
-  // Method to logout user
   logout() {
-    //confirm the logout from the current user
-    const confirmLogout = window.confirm('Are you sure you want to log out?');
+    const confirmLogout = window.confirm('are you sure, you want to logout');
     if (confirmLogout) {
       localStorage.removeItem('currentUser');
       this.router.navigate(['/login']);
